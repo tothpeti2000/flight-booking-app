3ab83ee879524bfb19ff111ca653d289949814e0
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-07-16 19:59:58 +0200

    FlightCards made prettier

--- frontend/src/components/booking/flights/FlightCard.vue
+++ frontend/src/components/booking/flights/FlightCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { FlightCardProps } from "@/interfaces/booking/flightCardProps";
 import { useBookingStore } from "@/store/store";
+import moment from "moment";
 import { computed } from "vue";
 
 const props = withDefaults(defineProps<FlightCardProps>(), {
@@ -20,72 +21,56 @@ const bgColor = computed(() => {
   return isChosen.value ? "bg-primary" : "surface-card";
 });
 
+const flightDate = moment(props.flight.departureTime).format("LLLL");
+const departureDate = moment(props.flight.departureTime).format("LT");
+const arrivalDate = moment(props.flight.arrivalTime).format("LT");
+
+const flightTime = computed(() => {
+  const hours = Math.floor(props.flight.flightTimeMinutes / 60);
+  const minutes = props.flight.flightTimeMinutes % 60;
+
+  return `${hours}h ${minutes}m`;
+});
+
 const handleClick = () => {
   const toSave = isChosen.value ? undefined : props.flight;
 
   saveFlightChoice(toSave, props.isReturn);
 };
-
-const arrival = computed(() => {
-  return props.flight.arrivalTime.toLocaleTimeString("hu-HU", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-});
-
-const departure = computed(() => {
-  return props.flight.departureTime.toLocaleTimeString("hu-HU", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-});
-
-const dayName = computed(() => {
-  return props.flight.departureTime.toLocaleString("en-us", {
-    weekday: "long",
-  });
-});
-
-const departDate = computed(() => {
-  return props.flight.departureTime.toLocaleString("hu-HU", {
-    dateStyle: "medium",
-  });
-});
-
-const flightTimeConverter = computed(() => {
-  const hour = Math.floor(props.flight.flightTimeMinutes / 60);
-  const minutes = props.flight.flightTimeMinutes % 60;
-  return hour + "h " + minutes + "min";
-});
 </script>
 
 <template>
   <Card @click="handleClick" :class="bgColor">
     <template #content>
-      <div class="flex flex-row">
-        <div class="flex flex-column align-items-start justify-content-end">
-          <div class="text-l">{{ dayName }}, {{ departDate }}</div>
-          <div class="text-5xl line-height-1 mb-1 mt-1">{{ arrival }}</div>
-          <div class="text-xl">
-            {{ flight.departureCity }} {{ flight.departureAirport }}
+      <div>{{ flightDate }}</div>
+
+      <div class="flex">
+        <div class="flex flex-column">
+          <div class="text-5xl font-semibold">
+            {{ departureDate }}
+          </div>
+          <div class="text-xl align-self-start">
+            {{ flight.departureCity }}
           </div>
         </div>
+
         <div
-          class="flex flex-column align-items-center justify-content-center flex-grow-1 text-l"
+          class="flex flex-column justify-content-center align-items-center flex-1"
         >
-          {{ flightTimeConverter }}
+          {{ flightTime }}
+          <i class="pi pi-arrow-right" />
         </div>
-        <div class="flex flex-column align-items-end justify-content-end">
-          <div class="text-5xl line-height-1 mb-1 mt-1">{{ departure }}</div>
-          <div class="text-xl">
-            {{ flight.arrivalCity }} {{ flight.arrivalAirport }}
+
+        <div class="flex flex-column">
+          <div class="text-5xl font-semibold">{{ arrivalDate }}</div>
+          <div class="text-xl align-self-end">
+            {{ flight.arrivalCity }}
           </div>
         </div>
-        <div
-          class="flex flex-column align-items-end justify-content-center ml-3"
-        >
+
+        <div class="flex flex-column justify-content-center">
           <div
-            class="bg-blue-400 ml-4 mr-3 pl-3 pr-3 p-2 text-2xl text-white border-round-md font-semibold w-auto"
+            class="bg-blue-400 text-white ml-4 p-2 text-2xl font-semibold border-round-md"
           >
             {{ flight.price }} Ft
           </div>
