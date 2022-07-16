8a99030edcac87455fc636d0ae31bce28fe91c73
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-07-16 14:03:20 +0200

    Airport interface added

--- frontend/src/interfaces/booking/start.ts
+++ frontend/src/interfaces/booking/start.ts
@@ -5,3 +5,12 @@ export interface BookingOptions {
   returnDate?: Date;
   passengerCount: number;
 }
+
+export interface Airport {
+  id: string;
+  name: string;
+  cityName: string;
+  latitude: number;
+  longitude: number;
+  blobUrl: string;
+}
